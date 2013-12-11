72a19a841a667fa20126db0c1fcd47a55cd50e27
Jesse Lingeman <jmlingeman@nyu.edu>
2013-12-11 17:44:35 -0500

    Added a bunch of stuff to analysis for breaking into different aggregates

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
 
 class Token:
 	def __init__(self, word, postag, gold_postag, nertag, gold_nertag):
@@ -59,17 +60,76 @@ def collate_errors(doc):
 			if not ner_c and not pos_c:
 				doc.both_incorrect.append(t)
 
-def report_errors(doc):
+def report_errors_4key(errors):
 	report = {}
-	for e in doc.both_incorrect:
+	for e in errors:
 		key = "{0} {1} {2} {3}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+		# key = "{2} {3}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+		# key = "{0} {1}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+
+
 		if key not in report:
 			report[key] = 0
 		report[key] += 1
 
-	print report
+	sorted_report = sorted(report.iteritems(), key=operator.itemgetter(1))
+	sorted_report.reverse()
+	return sorted_report
+
+def report_errors_nerkey(errors):
+	report = {}
+	for e in errors:
+		# key = "{0} {1} {2} {3}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+		key = "{2} {3}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+		# key = "{0} {1}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+
+
+		if key not in report:
+			report[key] = 0
+		report[key] += 1
+
+	sorted_report = sorted(report.iteritems(), key=operator.itemgetter(1))
+	sorted_report.reverse()
+	return sorted_report
+
+def report_errors_poskey(errors):
+	report = {}
+	for e in errors:
+		# key = "{0} {1} {2} {3}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+		# key = "{2} {3}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+		key = "{0} {1}".format(e.postag, e.gold_postag, e.nertag, e.gold_nertag)
+
+
+		if key not in report:
+			report[key] = 0
+		report[key] += 1
+
+	sorted_report = sorted(report.iteritems(), key=operator.itemgetter(1))
+	sorted_report.reverse()
+	return sorted_report
+
+def write_report(filename, report):
+	f = open(filename, 'w')
+	for tok in report:
+		f.write("\t".join(map(str, tok)) + "\n")
 
 doc = read_output_file("ner-tag-output.txt")
 collate_errors(doc)
-report_errors(doc)
 
+write_report("both_incorrect.txt", report_errors_4key(doc.both_incorrect))
+write_report("ner_incorrect.txt", report_errors_4key(doc.ner_incorrect))
+write_report("pos_incorrect.txt", report_errors_4key(doc.pos_incorrect))
+write_report("ner_correct_pos_incorrect.txt", report_errors_4key(doc.ner_correct_pos_incorrect))
+write_report("ner_incorrect_pos_correct.txt", report_errors_4key(doc.ner_incorrect_pos_correct))
+
+write_report("both_incorrect_nerkey.txt", report_errors_nerkey(doc.both_incorrect))
+write_report("ner_incorrect_nerkey.txt", report_errors_nerkey(doc.ner_incorrect))
+write_report("pos_incorrect_nerkey.txt", report_errors_nerkey(doc.pos_incorrect))
+write_report("ner_correct_pos_incorrect_nerkey.txt", report_errors_nerkey(doc.ner_correct_pos_incorrect))
+write_report("ner_incorrect_pos_correct_nerkey.txt", report_errors_nerkey(doc.ner_incorrect_pos_correct))
+
+write_report("both_incorrect_poskey.txt", report_errors_poskey(doc.both_incorrect))
+write_report("ner_incorrect_poskey.txt", report_errors_poskey(doc.ner_incorrect))
+write_report("pos_incorrect_poskey.txt", report_errors_poskey(doc.pos_incorrect))
+write_report("ner_correct_pos_incorrect_poskey.txt", report_errors_poskey(doc.ner_correct_pos_incorrect))
+write_report("ner_incorrect_pos_correct_poskey.txt", report_errors_poskey(doc.ner_incorrect_pos_correct))
